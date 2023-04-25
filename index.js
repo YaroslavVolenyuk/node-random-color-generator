59c753d4519b25437be6314045ed5431273963f9
Yaroslav Volenyuk <volenyuk.yaroslav@gmail.com>
2023-04-25 17:22:27 +0200

    Add color picker logic

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-const
+import chalk from 'chalk';
+import { ColorTranslator, Harmony, Mix } from 'colortranslator';
+import generateRandomColor from 'generate-random-color';
+import randomColor from 'randomcolor';
+
+const randColor = randomColor({
+  luminosity: `${process.argv[3]}`,
+  hue: `${process.argv[2]}`,
+});
+
+// console.log(randColor, `${process.argv[2]}`, `${process.argv[3]}`);
+// console.log(chalk.hex(`${randColor}`)(`${randColor} one more random color`));
+
+const randomHexColor = generateRandomColor.hex();
+//
+// const color = new ColorTranslator(`${randomHexColor}`);
+// const hslColor = color.HSL;
+// console.log(hslColor);
+
+// console.log(process.argv[2]);
+// const hex = new ColorTranslator(`${generateRandomColor}`);
+// console.log(hex);
+
+// console.log(chalk.hex(`${randomHexColor}`)(`${randomHexColor}`));
+
+//
+//
+if (process.argv[2] in chalk) {
+  console.log(
+    chalk.hex(`${randColor}`)(`
+
+  # ###############################
+  # ###############################
+  # ###############################
+  # #####                     #####
+  # #####        ${randColor}      #####
+  # #####                     #####
+  # ###############################
+  # ###############################
+  # ###############################
+
+  `),
+  );
+} else {
+  console.log(
+    chalk.hex(`${randomHexColor}`)(`
+  # ###############################
+  # ###############################
+  # ###############################
+  # #####                     #####
+  # #####       ${randomHexColor}       #####
+  # #####                     #####
+  # ###############################
+  # ###############################
+  # ###############################`),
+  );
+}
